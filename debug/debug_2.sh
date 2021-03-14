5276562d973f57a981e8a12db7e1a3a578f2095d
haroldosales <haroldognu@gmail.com>
2021-03-14 18:01:50 -0300

    atualizando

--- debug/debug_2.sh
+++ debug/debug_2.sh
@@ -1,27 +1,25 @@
 # ------------------------------- VARIÁVEIS ----------------------------------------- #
-CHAVE_DEBUG=0
-NIVEL_DEBUG=0
+#CHAVE_DEBUG=0
+#NIVEL_DEBUG=0
 # ------------------------------------------------------------------------ #
 
 # ------------------------------- FUNÇÕES ----------------------------------------- #
-Debugar () {
-  [ $1 -le $NIVEL_DEBUG ] && echo "Debug $* ------"
-}
-
+#Debugar () {
+#  [ $1 -le $NIVEL_DEBUG ] && echo "Debug $* ------"
+#}
+#
 Soma () {
   local total=0
 
   for i in $(seq 1 25); do
-      Debugar 1 "Entrei no for com o valor: $i"
+      #Debugar 1 "Entrei no for com o valor: $i"
       total=$(($total+$i))
-      Debugar 2 "Depois da soma: $total"
+    #  Debugar 2 "Depois da soma: $total"
   done
+  echo $total
 }
 # ------------------------------------------------------------------------ #
-case "$1" in
-  -d) [ $2 ] && NIVEL_DEBUG=$2 ;;
-   *) Soma                     ;;
-esac
+
 
 Soma
 # ------------------------------------------------------------------------ #
